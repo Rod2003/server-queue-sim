9bf33a4ebae428c5f1759b5959a37d709c5774ec
Rod2003 <rodrigo.daguila@gmail.com>
2024-02-16 19:31:48 -0500

    Initial commit

---
+++ simulation_v1.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+# initialize parameters time, packet arrival rate, packet service rate, and server queue
+time = 0
+
+# variable arrival rate; could take on these values: 0.2, 0.4, 0.5, 0.6, 0.65, 0.7, 0.72, 0.74, and 0.745
+arrivalRate = 0.2
+
+# fixed service rate
+serviceRate = 0.75
+
+queue = []
+
+'''
+while time <= 1000000:
+    # packet arrival
+    
+
+    # packet departure
+
+
+    # update time
+    time += 1
+'''
+
+# produce plot
+plt.plot([1,2,3,4])
+plt.ylabel('some numbers')
+plt.show()
